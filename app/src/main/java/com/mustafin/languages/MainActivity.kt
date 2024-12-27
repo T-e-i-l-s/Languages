ab405ada6c2af145b900c34fe79b58d58adf9543
kmous <kfmust@gmail.com>
2024-12-27 23:00:06 +0300

    Basic Structure

--- app/src/main/java/com/mustafin/languages/MainActivity.kt
+++ app/src/main/java/com/mustafin/languages/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.mustafin.languages.ui.theme.AppTheme
+import com.mustafin.languages.core.ui.theme.AppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
--- app/src/main/java/com/mustafin/languages/core/ui/theme/Type.kt
+++ app/src/main/java/com/mustafin/languages/core/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.mustafin.languages.ui.theme
+package com.mustafin.languages.core.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
