d027b0c8d8f0b99b2af8eef54ffa84aa38eb2299
kmous <kfmust@gmail.com>
2024-12-27 22:41:00 +0300

    Init

---
+++ app/src/main/java/com/mustafin/languages/ui/theme/Theme.kt
@@ -0,0 +1,14 @@
+package com.mustafin.languages.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AppTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        typography = Typography,
+        content = content
+    )
+}
---
+++ app/src/main/java/com/mustafin/languages/ui/theme/Type.kt
@@ -0,0 +1,36 @@
+package com.mustafin.languages.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.mustafin.languages.R
+
+val Typography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = FontFamily(Font(R.font.pacifico, FontWeight.Bold)),
+        fontSize = 44.sp
+    ),
+
+    titleMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.montserrat, FontWeight.SemiBold)),
+        fontSize = 20.sp
+    ),
+
+    titleSmall = TextStyle(
+        fontFamily = FontFamily(Font(R.font.montserrat, FontWeight.SemiBold)),
+        fontSize = 16.sp
+    ),
+
+    labelMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.montserrat, FontWeight.Normal)),
+        fontSize = 16.sp
+    ),
+
+    labelSmall = TextStyle(
+        fontFamily = FontFamily(Font(R.font.montserrat, FontWeight.Normal)),
+        fontSize = 12.sp
+    )
+)
